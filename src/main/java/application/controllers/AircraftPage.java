0711bf85f6011bb11c08745522d1daab7f7e7fa9
jie1311 <tommy911113@gmail.com>
2017-02-25 21:33:16 +1100

    new chart tool
    which is faster

--- src/main/java/application/controllers/AircraftPage.java
+++ src/main/java/application/controllers/AircraftPage.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/aircraft")
@@ -17,10 +16,6 @@ public class AircraftPage {
     public String getAircraft(Model model) {
         RestTemplate restTemplate = new RestTemplate();
         Aircraft[] aircrafts = restTemplate.getForObject("http://localhost:8080/getAircraft", Aircraft[].class);
-//        ArrayList aircraftList = new ArrayList<>();
-//        for (Aircraft aircraft : aircrafts) {
-//            aircraftList.add(aircraft.toString());
-//        }
         model.addAttribute("aircrafts", aircrafts);
         return "aircraft";
     }
